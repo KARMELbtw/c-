044e7c650e5acbdf3683c76902a5ee437e17a989
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2022-09-27 10:48:59 +0200

    Add files via upload

---
+++ pierwszy.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        int liczba = 0;
+        while(liczba <500)
+        {
+            liczba++;
+            Console.Write(liczba + " ");
+        }
+    }
+}
