91b877fb0492862bfdfcf338d8a292114686959e
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2022-11-14 19:28:47 +0100

    Add files via upload

---
+++ for.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    public static int dodaj(int x)
+    {
+        x+=5;
+            return x;
+    }
+
+    static void Main()
+    {
+    int wynik = 0;
+    Console.Write(dodaj (15));
+    }
+
+}
